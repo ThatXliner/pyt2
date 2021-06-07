4e111c9b6f69b3b37eb384ca18c82291a73821d4
Bryan Hu <bryan.hu.2020@gmail.com>
2021-06-07 15:20:40 -0700

    :sparkles: Post generation hooks!

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,21 @@
 import os
+import subprocess, shutil
 
 if "{{ cookiecutter.add_docs }}" == "no":
     os.rmdir("docs")
+
+if "yes" in {
+    '{{ cookiecutter["(post-generation hook) install venv?"] }}',
+    '{{ cookiecutter["(post-generation hook) run git init?"] }}',
+}:
+    print("Running post project hooks:")
+
+    if '{{ cookiecutter["(post-generation hook) install venv?"] }}' == "yes":
+        print(" - Running `poetry install`...", end=" ")
+        subprocess.run([shutil.which("poetry") or "poetry", "install"])
+        print("done")
+
+    if '{{ cookiecutter["(post-generation hook) run git init?"] }}' == "yes":
+        print(" - Running `git init`...", end=" ")
+        subprocess.run([shutil.which("git") or "git", "init"])
+        print("done")
