80d782a690c58bffdbb40af3d516512a23cf208a
ThatXliner <bryan.hu.2020@gmail.com>
2021-06-21 13:35:22 -0700

    :sparkles: Silence stdout/stderr

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,10 +12,10 @@ if "yes" in {
 
     if '{{ cookiecutter["(post-generation hook) install venv?"] }}' == "yes":
         print(" - Running `poetry install`...", end=" ")
-        subprocess.run([shutil.which("poetry") or "poetry", "install"])
+        subprocess.run([shutil.which("poetry") or "poetry", "install"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         print("done")
 
     if '{{ cookiecutter["(post-generation hook) run git init?"] }}' == "yes":
         print(" - Running `git init`...", end=" ")
-        subprocess.run([shutil.which("git") or "git", "init"])
+        subprocess.run([shutil.which("git") or "git", "init"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         print("done")
