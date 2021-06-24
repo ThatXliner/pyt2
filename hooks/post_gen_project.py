d11d7b91f5ba14720a32e41aa95e10608d130b8a
ThatXliner <bryan.hu.2020@gmail.com>
2021-06-23 21:29:15 -0700

    :recycle: :zap: Possibly improve speed?

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,13 +9,13 @@ if "yes" in {
     '{{ cookiecutter["(post-generation hook) run git init?"] }}',
 }:
     print("Running post project hooks:")
-
-    if '{{ cookiecutter["(post-generation hook) install venv?"] }}' == "yes":
-        print(" - Running `poetry install`...", end=" ")
-        subprocess.run([shutil.which("poetry") or "poetry", "install"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print("done")
-
-    if '{{ cookiecutter["(post-generation hook) run git init?"] }}' == "yes":
-        print(" - Running `git init`...", end=" ")
-        subprocess.run([shutil.which("git") or "git", "init"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print("done")
+{% if cookiecutter["(post-generation hook) install venv?"] == "yes" %}
+    print(" - Running `poetry install`...", end=" ")
+    subprocess.run([shutil.which("poetry") or "poetry", "install"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print("done")
+{% endif %}
+{% if cookiecutter["(post-generation hook) run git init?"] == "yes" %}
+    print(" - Running `git init`...", end=" ")
+    subprocess.run([shutil.which("git") or "git", "init"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print("done")
+{% endif %}
