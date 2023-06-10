b04e999d3acd879df5f7190c1fee76eb4b7a931a
ThatXliner <bryan.hu.2020@gmail.com>
2023-06-09 20:21:44 -0700

    :boom: Switch to copier

--- hooks/pre_gen_project.py
+++
@@ -1,9 +0,0 @@
-import re
-import sys
-
-# Validate module name
-MODULE_REGEX = re.compile(r"^[_a-zA-Z][_a-zA-Z0-9]+$")
-module_name = "{{ cookiecutter.module_name }}"
-if not MODULE_REGEX.match(module_name):
-    print("ERROR: %s is not a valid Python module name!" % module_name)
-    sys.exit(1)
--- {{ cookiecutter.pypi_name }}/{{ cookiecutter.module_name }}/__init__.py
+++
@@ -1,2 +0,0 @@
-"""{{ cookiecutter.project_description }}"""
-__version__ = "0.1.0"
